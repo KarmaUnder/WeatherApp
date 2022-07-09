de210943c0e4e1d8b09d7a42b8d8ea640d182e5b
Yoel Larrosa <Yoel.larrosa@hotmail.com>
2022-07-09 00:49:06 -0300

    Proyecto completado
    Proyecto antiguo subido para documentacion

---
+++ src/components/loading.jsx
@@ -0,0 +1,12 @@
+import styles from './loading.module.css'
+export default function loading(){
+    return (
+    <div className={styles.loadingContainer}>
+        <div className={styles.loader}>
+            <div>
+
+            </div>
+        </div>
+    </div>
+    )
+}
